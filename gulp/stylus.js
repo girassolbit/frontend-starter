9a0a7e88f75e84fab1ee23a7e1beb89654cbc914
Charleno Pires <charleno@gmail.com>
2015-05-26 02:08:42 -0300

    Preparing Gulp toolset automatize

--- gulp/stylus.js
+++ gulp/stylus.js
@@ -1,13 +1,40 @@
 (function(){
   'use strict';
 
-  var nib = require('nib');
+  var
+      nib       = require('nib')
+      ,jeet     = require('jeet')
+      ,rupture  = require('rupture')
+      ,kouto_swiss = require('kouto-swiss')
+      ,autoprefixer = require('autoprefixer-stylus')
+      ,axis = require('axis')
+      ;
 
-  module.exports = function (CSS_FOLDER, CSS_COMPILED, gulp, stylus) {
+  module.exports = function (CSS_FOLDER, CSS_COMPILED, gulp, stylus, plumber, reload) {
     gulp.task('stylus', function(){
-      gulp.src(CSS_FOLDER + "*.styl")
-        .pipe(stylus({ use: [nib()] }))
+      gulp.src(CSS_FOLDER + '**/*.styl')
+        .pipe(plumber({
+          handleError: function (error) {
+              console.log(error);
+              this.emit('end');
+          }
+        }))
+        .pipe(stylus({
+           errors: true
+          ,linenos: true
+          ,compress: true
+          use: [
+            jeet()
+            ,nib()
+            ,rupture()
+            ,autoprefixer({ browsers: ['last 2 versions', 'ie >= 8'] })
+            ,axis()
+            ,koutoSwiss()
+          ]
+          ,import: ['nib', 'jeet', 'rupture', 'kouto-swiss']
+        }))
         .pipe(gulp.dest(CSS_COMPILED))
+        .pipe(reload({stream:true}));
     });
   };
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,36 @@
-/*
-"gulp": "gulp-jshint": "gulp-csslint": "gulp-jslint":
-"gulp-stylus": "gulp-uglify": "gulp-notify":
-"gulp-livereload": "gulp-traceur": "gulp-minify-html":
-"gulp-minify-css": "gulp-imagemin": "gulp-concat":
-*/
-
-var gulp        = require('gulp'),
-    stylus      = require('gulp-stylus'),
-    csslint     = require('gulp-csslint'),
-    jshint      = require('gulp-jshint'),
-    jslint      = require('gulp-jslint'),
-    uglify      = require('gulp-uglify'),
-    notify      = require('gulp-notify'),
-    livereload  = require('gulp-livereload'),
-    traceur     = require('gulp-traceur'),
-    minify-html = require('gulp-minify-html'),
-    minify-css  = require('gulp-minify-css'),
-    imagemin    = require('gulp-imagemin'),
-    concat      = require('gulp-concat'),
-    // prefixer  = require('gulp-autoprefixer'),
+var
+    gulp         = require('gulp')
+    ,stylus      = require('gulp-stylus')
+    ,csslint     = require('gulp-csslint')
+    ,jshint      = require('gulp-jshint')
+    ,jslint      = require('gulp-jslint')
+    ,uglify      = require('gulp-uglify')
+    ,notify      = require('gulp-notify')
+    // ,livereload  = require('gulp-livereload')
+    ,traceur     = require('gulp-traceur')
+    ,minify_html = require('gulp-minify-html')
+    ,minify_css  = require('gulp-minify-css')
+    ,imagemin    = require('gulp-imagemin')
+    ,concat      = require('gulp-concat')
+    // ,prefixer    = require('gulp-autoprefixer')
+    ,plumber     = require('gulp-plumber')
+    // ,webserver   = require('gulp-webserver')
+    ,browserSync = require('browser-sync')
+    ,reload      = browserSync.reload
     ;
 
-var JS_FOLDER = './public/js/',
-    CSS_FOLDER = './public/css/',
-    CSS_COMPILED = CSS_FOLDER + 'compiled/';
+var JS_FOLDER = './public/js/'
+    ,CSS_FOLDER = './public/css/'
+    ,JS_COMPILED = JS_FOLDER + 'compiled/'
+    ,CSS_COMPILED = CSS_FOLDER + 'compiled/'
+    ;
 
-require('./gulp/stylus.js')(CSS_FOLDER, CSS_COMPILED, gulp, stylus);
+require('./gulp/stylus.js')(CSS_FOLDER, CSS_COMPILED, gulp, stylus, plumber, reload);
 
-gulp.task('default', function() {
-  gulp.start('stylus');
-	// gulp.start('jshint');
-	// gulp.start('csslint');
-});
+gulp.task('default',
+  [
+    'stylus'
+    ,'csslint'
+    ,'jshint'
+    ,'watch'
+]);
